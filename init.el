ca9a38c454dd631372d9b1d498fd9955e5fb3cb4
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-08-05 16:57:48 +0200

    Fix for paredit.

--- init.el
+++ init.el
@@ -234,6 +234,7 @@
   (add-hook 'slime-mode-hook 'use-paredit)
   (add-hook 'slime-repl-mode-hook 'use-paredit)
   :config
+  (require 'cl); Seems to be necessary
   (slime-setup '(slime-company slime-fancy))
   (setq inferior-lisp-program "/usr/bin/sbcl"))
 

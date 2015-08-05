22e749c4ca16d8b0a9df3f8874ba0e0f851abe4e
Marco Schlumpp <marco.schlumpp@gmail.com>
2015-08-05 16:57:36 +0200

    Added hydra.

--- init.el
+++ init.el
@@ -93,6 +93,39 @@
   `(eval-after-load ,mode
      '(progn ,@body)))
 
+;;;; Some functions
+(defun hydra-move-splitter-left (arg)
+  "Move window splitter left."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+        (windmove-find-other-window 'right))
+      (shrink-window-horizontally arg)
+    (enlarge-window-horizontally arg)))
+
+(defun hydra-move-splitter-right (arg)
+  "Move window splitter right."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+        (windmove-find-other-window 'right))
+      (enlarge-window-horizontally arg)
+    (shrink-window-horizontally arg)))
+
+(defun hydra-move-splitter-up (arg)
+  "Move window splitter up."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+        (windmove-find-other-window 'up))
+      (enlarge-window arg)
+    (shrink-window arg)))
+
+(defun hydra-move-splitter-down (arg)
+  "Move window splitter down."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+        (windmove-find-other-window 'up))
+      (shrink-window arg)
+    (enlarge-window arg)))
+
 ;;;; Paredit
 (defun use-paredit ()
   (electric-pair-mode -1)
@@ -310,3 +343,41 @@
   :commands alchemist-mode
   :config
   (setq alchemist-key-command-prefix (kbd "C-c f")))
+
+;;;; Hydra
+(use-package hydra
+  :config
+  (defhydra hydra-window (global-map "C-#" :hint nil)
+    "
+  ^ Movement ^  │    ^  Splits  ^    │  ^ Buffers  ^  │  ^  Other ^
+─^─────^─┴──^─────^──┴─^─────^─┴─^────^─
+ [_h_] ←          [_u_] vertical        [_b_] switch      [_z_] undo
+ [_j_] ↓          [_i_] horizontal      [_f_] find file   [_Z_] redo
+ [_k_] ↑          [_H_] grow left
+ [_l_] →          [_L_] grow right
+  ^ ^              [_K_] grow up
+  ^ ^              [_J_] grow down
+ [_SPC_] quit      [_d_] close
+"
+    ("h" windmove-left)
+    ("j" windmove-down)
+    ("k" windmove-up)
+    ("l" windmove-right)
+    ("u" (lambda ()
+           (interactive)
+           (split-window-right)
+           (windmove-right)))
+    ("i" (lambda ()
+           (interactive)
+           (split-window-below)
+           (windmove-down)))
+    ("H" hydra-move-splitter-left)
+    ("L" hydra-move-splitter-right)
+    ("K" hydra-move-splitter-up)
+    ("J" hydra-move-splitter-down)
+    ("d" delete-window)
+    ("b" ido-switch-buffer)
+    ("f" ido-find-file)
+    ("z" winner-undo)
+    ("Z" winner-redo)
+    ("SPC" nil)))

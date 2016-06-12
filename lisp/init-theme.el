b152e8cb6c48e2d168c563f23672defe679efa3b
Marco Schlumpp <marco.schlumpp@gmail.com>
2016-06-12 18:52:24 +0200

    Ensure material-theme.

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -2,6 +2,7 @@
 (add-to-list 'custom-theme-load-path (concat user-emacs-directory (convert-standard-filename "themes/")))
 
 (use-package material-theme
+  :ensure t
   :config
   (load-theme 'material-light t))
 

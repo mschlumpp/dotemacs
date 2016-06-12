d452a1abcf50f640fa73076977def8aae599d566
Marco Schlumpp <marco.schlumpp@gmail.com>
2016-06-12 18:07:06 +0200

    Switch to material theme.

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,8 +1,8 @@
 ;; Add custom theme path
 (add-to-list 'custom-theme-load-path (concat user-emacs-directory (convert-standard-filename "themes/")))
 
-(use-package zerodark-theme
+(use-package material-theme
   :init
-  (load-theme 'zerodark t))
+  (load-theme 'material-light t))
 
 (provide 'init-theme)

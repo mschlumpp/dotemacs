01aeb002b04a391a32ab0222dd25aa646999c6df
Marco Schlumpp <marco.schlumpp@gmail.com>
2016-09-02 14:00:27 +0200

    Add doom one to load path.
    The themes need to load doom.el and thus the directory needs to be in
    the load path.

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,6 +1,7 @@
 ;; Add custom theme path
 (add-to-list 'custom-theme-load-path (concat user-emacs-directory (convert-standard-filename "themes/")))
 (add-to-list 'custom-theme-load-path (concat user-emacs-directory (convert-standard-filename "themes/doom-one")))
+(add-to-list 'load-path (concat user-emacs-directory (convert-standard-filename "themes/doom-one")))
 
 (use-package material-theme
   :ensure t

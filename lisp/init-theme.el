e0c7a8542634ce73a964836b521fa9aa73bba3a1
Marco Schlumpp <marco.schlumpp@gmail.com>
2016-09-02 13:54:00 +0200

    Add doom one theme.

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,5 +1,6 @@
 ;; Add custom theme path
 (add-to-list 'custom-theme-load-path (concat user-emacs-directory (convert-standard-filename "themes/")))
+(add-to-list 'custom-theme-load-path (concat user-emacs-directory (convert-standard-filename "themes/doom-one")))
 
 (use-package material-theme
   :ensure t

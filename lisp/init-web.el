7ed05cf500d8367a6fc6843af20ad09d180b818c
Marco Schlumpp <marco.schlumpp@gmail.com>
2016-09-02 13:31:35 +0200

    Use web-mode for HTML/CSS.

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -10,11 +10,12 @@
   (add-hook 'typescript-mode-hook 'xy//setup-tide))
 
 (use-package web-mode
+  :mode ("\\.tsx\\'" "\\.html\\'" "\\.css\\'")
   :init
   (when (package-installed-p 'tide)
-    (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
     (add-hook 'web-mode-hook
               (lambda ()
+                (electric-pair-mode -1)
                 (when (string-equal "tsx" (file-name-extension buffer-file-name))
                   (xy//setup-tide))))))
 

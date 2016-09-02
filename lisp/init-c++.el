cfd449e2d2606e305f4f2dab9eb92171652e994f
Marco Schlumpp <marco.schlumpp@gmail.com>
2016-09-02 13:45:38 +0200

    Fix formatting for C++11 lambdas.

--- lisp/init-c++.el
+++ lisp/init-c++.el
@@ -11,7 +11,20 @@
         c-basic-offset 4
         c-cleanup-list '(defun-close-semi
                           scope-operator
-                          empty-defun-braces)))
+                          empty-defun-braces))
+
+  (defadvice c-lineup-arglist (around lambda-lineup activate)
+    "Improve indentation of continued C++11 lambda function opened as argument."
+    (setq ad-return-value
+          (if (and (equal major-mode 'c++-mode)
+                   (ignore-errors
+                     (save-excursion
+                       (goto-char (c-langelem-pos langelem))
+                       ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+                       ;;   and with unclosed brace.
+                       (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+              0                           ; no additional indent
+            ad-do-it))))
 
 (use-package fic-mode
   :commands (turn-on-fic-mode)

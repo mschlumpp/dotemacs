811d5a09a7ce2faa2801bc317b4d7708d8c8a9f6
Marco Schlumpp <marco.schlumpp@gmail.com>
2016-09-02 13:43:32 +0200

    Fix racer completion.

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,11 +1,13 @@
 ;;;; Rust
 (use-package racer
+  :diminish "ⓡ"
   :init
   (add-hook 'rust-mode-hook #'(lambda ()
-                                (racer-activate)
-                                (racer-turn-on-eldoc)
+                                ;; Disable ycmd because it doesn't work with racer...
+                                (ycmd-mode -1)
+                                (racer-mode 1)
+                                (eldoc-mode 1)
                                 (local-set-key (kbd "M-.") #'racer-find-definition)
-                                (local-set-key (kbd "TAB") #'racer-complete-or-indent))))
-
+                                (local-set-key (kbd "TAB") #'company-indent-or-complete-common))))
 
 (provide 'init-rust)

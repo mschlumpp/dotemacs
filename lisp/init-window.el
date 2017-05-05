a49b96e43a550fc2039629ca46400696b99f654a
Marco Schlumpp <marco.schlumpp@gmail.com>
2017-05-05 13:08:48 +0200

    Add cargo build window to popwin.

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -123,6 +123,7 @@ print:   [_c_] class hierarchy"
   (global-set-key (kbd "C-c w") popwin:keymap)
   (add-to-list 'popwin:special-display-config '("*RTags*" :stick t :noselect t :position bottom :width 60))
   (add-to-list 'popwin:special-display-config '("*Cargo Test*" :noselect t))
+  (add-to-list 'popwin:special-display-config '("*Cargo Build*" :noselect t))
   (add-to-list 'popwin:special-display-config '("*Flycheck errors*" :noselect t)))
 
 (provide 'init-window)

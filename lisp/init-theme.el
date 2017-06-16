5f9dbc5950dd81cb5ff1819c5dc912afdf8de704
Marco Schlumpp <marco.schlumpp@gmail.com>
2017-06-16 20:45:20 +0200

    Remove doom one theme submodule.

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,7 +1,5 @@
 ;; Add custom theme path
 (add-to-list 'custom-theme-load-path (concat user-emacs-directory (convert-standard-filename "themes/")))
-(add-to-list 'custom-theme-load-path (concat user-emacs-directory (convert-standard-filename "themes/doom-one")))
-(add-to-list 'load-path (concat user-emacs-directory (convert-standard-filename "themes/doom-one")))
 
 (req-package material-theme
   :config

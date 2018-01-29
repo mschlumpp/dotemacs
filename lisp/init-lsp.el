95b545ceb2ca38e3652af31bc29c2e0296e93eb9
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-01-29 20:36:41 +0100

    Use the awesome lsp-ui

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,5 +1,13 @@
 (req-package lsp-mode)
 
+(req-package lsp-ui
+  :require lsp-mode
+  :init
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
+
 (req-package company-lsp
   :require lsp-mode
   :init

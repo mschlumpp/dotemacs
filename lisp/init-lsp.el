b58b18311ecef2ee1693cc5de552271f0a87adb0
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-03-03 15:13:21 +0100

    Add vi keybindings for lsp-ui-peek

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -7,6 +7,10 @@
   :config
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  (define-key lsp-ui-peek-mode-map (kbd "k") #'lsp-ui-peek--select-prev)
+  (define-key lsp-ui-peek-mode-map (kbd "j") #'lsp-ui-peek--select-next)
+  (define-key lsp-ui-peek-mode-map (kbd "h") #'lsp-ui-peek--select-prev-file)
+  (define-key lsp-ui-peek-mode-map (kbd "l") #'lsp-ui-peek--select-next-file)
   (setq lsp-ui-sideline-show-hover nil))
 
 (req-package company-lsp

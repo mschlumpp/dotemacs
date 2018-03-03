89892cad7f94d4971ac476a4835aad66375e293a
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-03-03 09:53:10 +0100

    Don't show verbose hover informations in sideline

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -6,7 +6,8 @@
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
   :config
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  (setq lsp-ui-sideline-show-hover nil))
 
 (req-package company-lsp
   :require lsp-mode

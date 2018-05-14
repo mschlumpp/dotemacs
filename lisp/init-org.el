683cccbf6c2151deef6756b4c502335af937f1c6
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-05-14 11:38:51 +0200

    Enable visual line mode for org-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,6 +3,7 @@
   :bind (("C-c g" . org-agenda)
          ("C-c c" . org-capture))
   :init
+  (add-hook 'org-mode-hook 'visual-line-mode)
   (add-hook 'org-mode-hook 'org-indent-mode)
   (setq org-directory "~/orgmode")
   (setq org-default-notes-file (concat org-directory "/notes.org")))

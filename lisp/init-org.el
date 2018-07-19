01945509e6e54a8104d74b3402f938a8c25d6cc8
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-07-19 12:22:32 +0200

    Scale latex preview fragments in org-mode on hidpi laptop

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -6,6 +6,9 @@
   (add-hook 'org-mode-hook 'visual-line-mode)
   (add-hook 'org-mode-hook 'org-indent-mode)
   (setq org-directory "~/orgmode")
-  (setq org-default-notes-file (concat org-directory "/notes.org")))
+  (setq org-default-notes-file (concat org-directory "/notes.org"))
+  :config
+  (when (equal (system-name) "fedtop")
+    (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))))
 
 (provide 'init-org)

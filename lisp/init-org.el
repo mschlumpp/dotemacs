44bd9cdf7251cb0fd6b19ab3c1e17d96340a3c1b
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-07-19 12:42:53 +0200

    Fix org-mode configuration

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,13 +1,13 @@
-(req-package org-mode
+(req-package org
   :ensure nil
   :bind (("C-c g" . org-agenda)
          ("C-c c" . org-capture))
   :init
   (add-hook 'org-mode-hook 'visual-line-mode)
   (add-hook 'org-mode-hook 'org-indent-mode)
+  :config
   (setq org-directory "~/orgmode")
   (setq org-default-notes-file (concat org-directory "/notes.org"))
-  :config
   (when (equal (system-name) "fedtop")
     (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))))
 

c8f7541bc8e26d1075f6fd4e54d94de1eeb2de48
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-09-11 20:49:56 +0200

    Don't enable show-trailing-whitespace for all buffers

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -23,8 +23,13 @@
      (set-face-attribute face nil :weight 'normal :underline nil))
    (face-list)))
 
-(setq-default show-trailing-whitespace t
-              echo-keystrokes 0.5)
+(setq-default echo-keystrokes 0.5)
+
+(defun xy//show-trailing-whitespace ()
+  (setq-local show-trailing-whitespace t))
+
+(add-hook 'prog-mode-hook 'xy//show-trailing-whitespace)
+(add-hook 'conf-mode-hook 'xy//show-trailing-whitespace)
 
 (req-package beacon
   :diminish beacon-mode

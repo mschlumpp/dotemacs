d52dfb54ac6722cd36b601234170e8fc2ac3ca44
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-09-29 16:34:34 +0200

    Set cargo keybinds only for rust-mode and bind a few more

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -12,10 +12,18 @@
 (req-package cargo
   :require rust-mode
   :init
-  (evil-leader/set-key
+  (evil-leader/set-key-for-mode 'rust-mode
     "mt" 'cargo-process-test
+    "mc" 'cargo-process-current-file-tests
+    "mC" 'cargo-process-current-test
+
     "mm" 'cargo-process-check
     "mb" 'cargo-process-build
+    "ml" 'cargo-process-clippy
+
+    "md" 'cargo-process-doc
+    "mD" 'cargo-process-doc-open
+
     "mr" 'cargo-process-run))
 
 (provide 'init-rust)

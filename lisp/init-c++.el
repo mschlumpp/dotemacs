09a8f27289be1b9639cb3523462f4a8c68027715
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-11-21 11:42:07 +0100

    Use inlambda style parameter for C++ lambdas

--- lisp/init-c++.el
+++ lisp/init-c++.el
@@ -5,26 +5,14 @@
 		 (c-offsets-alist
 		  (topmost-intro-cont . 0)
 		  (access-label . -)
+                  (inlambda . 0)
 		  (inclass . +))))
   (setq c-default-style "unknown"
 	indent-tabs-mode 0
 	c-basic-offset 4
 	c-cleanup-list '(defun-close-semi
 			  scope-operator
-			  empty-defun-braces))
-
-  (defadvice c-lineup-arglist (around lambda-lineup activate)
-    "Improve indentation of continued C++11 lambda function opened as argument."
-    (setq ad-return-value
-	  (if (and (equal major-mode 'c++-mode)
-		   (ignore-errors
-		     (save-excursion
-		       (goto-char (c-langelem-pos langelem))
-		       ;; Detect "[...](" or "[...]{". preceded by "," or "(",
-		       ;;   and with unclosed brace.
-		       (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
-	      0                           ; no additional indent
-	    ad-do-it))))
+			  empty-defun-braces)))
 
 (req-package cpputils-cmake
   :commands cppcm-reload-all

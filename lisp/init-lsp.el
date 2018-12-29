737e7bd296b03a2ea9a56bd9e14ecd5364cf11af
Marco Schlumpp <marco.schlumpp@gmail.com>
2018-12-29 12:35:48 +0100

    Prefer flycheck in lsp and disable sideline

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -2,6 +2,7 @@
   :config
   (add-hook 'lsp-mode-hook 'flycheck-mode-on-safe)
   (add-hook 'lsp-mode-hook 'yas-minor-mode)
+  (setq lsp-prefer-flymake nil)
   (evil-leader/set-key
     "mff" 'lsp-format-buffer
     "mfr" 'lsp-format-region
@@ -12,13 +13,16 @@
   :init
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
   :config
+  (setq lsp-ui-sideline-enable nil)
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
   (define-key lsp-ui-peek-mode-map (kbd "k") #'lsp-ui-peek--select-prev)
   (define-key lsp-ui-peek-mode-map (kbd "j") #'lsp-ui-peek--select-next)
   (define-key lsp-ui-peek-mode-map (kbd "h") #'lsp-ui-peek--select-prev-file)
   (define-key lsp-ui-peek-mode-map (kbd "l") #'lsp-ui-peek--select-next-file)
-  (setq lsp-ui-sideline-show-hover nil))
+
+  (require 'lsp-ui-flycheck)
+  (add-hook 'lsp-after-open-hook (lambda () (lsp-ui-flycheck-enable 1))))
 
 (req-package company-lsp
   :require lsp-mode
